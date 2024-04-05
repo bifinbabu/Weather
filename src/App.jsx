8e31357ab5aced3e6307139d6911589e72be7b46
bifinbabu <bifinbabu088@gmail.com>
2024-04-05 23:16:16 +0530

    Router changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Forecast from "./Pages/Forecast";
 import Home from "./Pages/Home";
 import { pages } from "./Pages/config";
